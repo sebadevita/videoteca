2589bd7adb544485797d7c2d1f5522d0e8b7575a
Sebastian De Vita <sebassdevita@gmail.com>
2021-03-17 09:44:47 -0300

    Empezadas las validaciones del formulario de videos

--- frontend/src/componentes/Videos/VideoForm.tsx
+++ frontend/src/componentes/Videos/VideoForm.tsx
@@ -1,65 +1,87 @@
-import React, { ChangeEvent, FormEvent, useState, useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import { toast } from "react-toastify";
-import { Video } from "./Video";
-import * as VideoService from "./VideoService";
+import React, { ChangeEvent, FormEvent, useState, useEffect } from "react"
+import { useHistory, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
+import { Video } from "./Video"
+import * as VideoService from "./VideoService"
 
 type InputChange =
   | ChangeEvent<HTMLInputElement>
-  | ChangeEvent<HTMLTextAreaElement>;
+  | ChangeEvent<HTMLTextAreaElement>
 
 interface Params {
-  id: string;
+  id: string
 }
 
 const VideoForm = () => {
-  const history = useHistory();
-  const params = useParams<Params>();
+  const history = useHistory()
+  const params = useParams<Params>()
 
   const estadoInicial = {
     titulo: "",
     descripcion: "",
     url: "",
-  };
+  }
 
-  const [video, setVideo] = useState<Video>(estadoInicial);
+  const [video, setVideo] = useState<Video>(estadoInicial)
+  const [error, setError] = useState('')
+  const [fallo, setFallo] = useState(false)
+
+  const validarTitulo = () => {
+    if (!video.titulo) {
+      setFallo(true)
+      setError("El campo título es obligatorio")
+      
+    }
+  }
+  
+  const validarUrl = () => {
+    if (!video.url) {
+      setFallo(true)
+      setError("El campo URL es obligatorio")
+      
+    }
+  }
+
+  const validarErrores = () => {
+    validarTitulo()
+    validarUrl()
+  }
 
   const handleInputChange = (e: InputChange) => {
-    setVideo({ ...video, [e.target.name]: e.target.value });
-  };
+    setVideo({ ...video, [e.target.name]: e.target.value })
+  }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!params.id){
-      
-    await VideoService.crearVideo(video);
-    toast.success("El video se subió correctamente!");
-    setVideo(estadoInicial);
-    } else{
+    e.preventDefault()
+    validarErrores()
+    if (!params.id) {
+      await VideoService.crearVideo(video)
+      toast.success("El video se subió correctamente!")
+      setVideo(estadoInicial)
+    } else {
       await VideoService.actualizarVideo(params.id, video)
-      history.push('/')
+      history.push("/")
     }
-  };
+  }
 
   const getVideo = async (id: string) => {
-    const res = await VideoService.getVideo(id);
-    const { titulo, descripcion, url } = res.data;
-    setVideo({ titulo, descripcion, url });
-  };
+    const res = await VideoService.getVideo(id)
+    const { titulo, descripcion, url } = res.data
+    setVideo({ titulo, descripcion, url })
+  }
 
   useEffect(() => {
-    if (params.id) getVideo(params.id);
-  }, []);
+    if (params.id) getVideo(params.id)
+  }, [])
 
   return (
     <div className="row">
       <div className="col-md-4 offset-md-4">
         <div className="card">
           <div className="card-body">
-            {params.id ? 
-            <h3>Actualizar video</h3> 
-            : 
-            <h3>Subir video</h3>}
+            {params.id ? <h3>Actualizar video</h3> : <h3>Subir video</h3>}
+
+            
 
             <form onSubmit={handleSubmit}>
               <div className="form-group">
@@ -82,7 +104,6 @@ const VideoForm = () => {
                   className="form-control"
                   onChange={handleInputChange}
                   value={video.url}
-                  autoFocus
                 ></input>
               </div>
 
@@ -97,6 +118,14 @@ const VideoForm = () => {
                 ></textarea>
               </div>
 
+              {
+              fallo ? (
+                <span>{error}</span>
+              ):
+
+                <span></span>
+            }
+
               {params.id ? (
                 <button className="btn btn-secondary">Actualizar video</button>
               ) : (
@@ -107,7 +136,7 @@ const VideoForm = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default VideoForm;
+export default VideoForm
